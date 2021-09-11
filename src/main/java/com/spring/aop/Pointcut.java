08ebca567f555f4fff841483f95c56c34c83901b
StellarisJAY <760409558@qq.com>
2021-09-11 21:04:17 +0800

    feat: 添加AOP相关代码

---
+++ src/main/java/com/spring/aop/Pointcut.java
@@ -0,0 +1,15 @@
+package com.spring.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Jay
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Pointcut {
+    String value();
+}
