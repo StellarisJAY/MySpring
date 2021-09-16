ac75e2a3900d9d3f903d254170d2b5297cc8fd56
StellarisJAY <760409558@qq.com>
2021-09-16 19:17:01 +0800

    更新：属性注入以及循环依赖解决

--- src/main/java/com/spring/bean/BeanRegistry.java
+++ src/main/java/com/spring/bean/BeanRegistry.java
@@ -70,6 +70,7 @@ public class BeanRegistry {
     protected void addBeanDefinition(String beanName, BeanDefinition beanDefinition){
         synchronized (beanDefinitionMap){
             BeanDefinition oldBeanDefinition = beanDefinitionMap.get(beanName);
+            // 判断beanName是否重复
             if(oldBeanDefinition != null){
                 throw new IllegalStateException("无法注册名为：" + beanName + "的BeanDefinition，因为beanName已经被 " + oldBeanDefinition + " 使用");
             }
@@ -84,6 +85,14 @@ public class BeanRegistry {
      * @return bean对象
      */
     protected Object createBean(String beanName, BeanDefinition beanDefinition) {
+        /*
+            判断单例池是否已经有该bean，有则结束创建
+            因为在componentScan阶段就已经p排除了重复的beanName
+            所以这里的判断是在排除在autowired或构造器注入完成创建的bean
+         */
+        if(singletonObjects.containsKey(beanName)){
+            return singletonObjects.get(beanName);
+        }
         Class<?> beanClass = beanDefinition.getBeanClass();
         // 设置bean inCreation
         inCreation.add(beanName);
@@ -95,7 +104,8 @@ public class BeanRegistry {
             // 实例化
             instance = createInstance(constructor);
             // 三级缓存记录原始对象，beanName，beanDefinition
-            singletonFactories.put(beanName, ()->registerEarlySingleton(beanName, beanDefinition, instance));
+            //singletonFactories.put(beanName, ()->registerEarlySingleton(beanName, beanDefinition, instance));
+            singletonFactories.put(beanName, ()->instance);
         } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
             throw new RuntimeException("创建bean：" + beanName + "异常，无法实例化");
         }
@@ -160,19 +170,30 @@ public class BeanRegistry {
     private Object createInstance(Constructor<?> constructor) throws IllegalAccessException, InvocationTargetException, InstantiationException {
         Object[] parameters = new Object[constructor.getParameterCount()];
         int index = 0;
+        // 遍历构造方法参数，依次注入
         for (Parameter parameter : constructor.getParameters()) {
-            //byName
-            String beanName = parameter.getName();
+            String typeName = parameter.getType().getSimpleName();
+            /*
+                编译成字节码后，方法的参数名会被简化为arg0，arg1
+                所以无法直接通过param.getName来作为注入beanName
+                需要将类型名作为beanName
+             */
+            String beanName = Character.toLowerCase(typeName.charAt(0)) + typeName.substring(1);
+            System.out.println(beanName);
             // 从单例池中取对象
             if(singletonObjects.containsKey(beanName)){
+                // 能从单例池获取到bean，直接注入
                 parameters[index++] = singletonObjects.get(beanName);
             }
             /*
                 实例化阶段循环依赖检测
+                判断该beanName是否处于创建阶段
+                如果处于创建阶段，表示实例化阶段出现循环依赖
              */
             else if(inCreation.contains(beanName)){
                 throw new RuntimeException("实例化 " + beanName + " 发生循环依赖");
             }
+            // 目标bean未在创建中，调用createBean创建目标bean
             else{
                 BeanDefinition beanDefinition = getBeanDefinition(beanName);
                 // 创建bean
@@ -202,6 +223,7 @@ public class BeanRegistry {
             // 从二级缓存获取early singleton
             singletonObject = earlySingletonObjects.get(beanName);
             if(singletonObject == null){
+                // 加锁，保证put和remove的原子性
                 synchronized (singletonObjects){
                     // 从第三级缓存获取 bean创建方法
                     ObjectFactory objectFactory = singletonFactories.get(beanName);
@@ -227,17 +249,21 @@ public class BeanRegistry {
         Field[] fields = beanDefinition.getBeanClass().getDeclaredFields();
         for (Field field : fields) {
             if(field.isAnnotationPresent(Autowired.class)){
+                // 被注入的beanName
                 String autowireBeanName = field.getName();
+                // 被注入的beanDefinition
                 BeanDefinition autowireBeanDef = getBeanDefinition(autowireBeanName);
                 // 类型不匹配
                 if(field.getType() != autowireBeanDef.getBeanClass()){
                     throw new RuntimeException("需要的bean类型为 " + field.getType() + " ，找到的类型：" + autowireBeanDef.getBeanClass() + "不匹配");
                 }
+                // 尝试从单例池获取bean
                 Object autowireBean = singletonObjects.get(autowireBeanName);
+                // 获取失败，表示被注入的bean还没完成创建
                 if(autowireBean == null){
-                    // 正在创建的非懒加载单例
-                    if(autowireBeanDef.getScope() == BeanDefinition.SINGLETON && !autowireBeanDef.isLazyInit() && inCreation.contains(beanName)){
-                        // 使用getSingleton从三级缓存寻找
+                    // 正在创建的非懒加载单例,发生循环依赖
+                    if(autowireBeanDef.getScope() == BeanDefinition.SINGLETON && !autowireBeanDef.isLazyInit() && inCreation.contains(autowireBeanName)){
+                        // 使用getSingleton从三个缓存寻找
                         autowireBean = getSingleton(autowireBeanName);
                     }
                     else{
@@ -261,5 +287,9 @@ public class BeanRegistry {
     private Object registerEarlySingleton(String beanName, BeanDefinition beanDefinition, Object instance){
         // do aop proxy here if necessary
         return instance;
+    }
+
+    private boolean isSingleton(BeanDefinition beanDefinition){
+        return beanDefinition.getScope() == BeanDefinition.SINGLETON;
     }
 }
